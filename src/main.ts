21f86dd8b87a981acfa1be36c67e93aebfb4dc8a
Ettiene Mare <ettienemare@gmail.com>
2018-02-06 18:47:33 +0200

    Cleanup the code

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/modules/app/app.module';
 import { environment } from './environments/environment';
+import {PaperModule} from "./app/modules/paper/paper.module";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(PaperModule)
   .catch(err => console.log(err));
