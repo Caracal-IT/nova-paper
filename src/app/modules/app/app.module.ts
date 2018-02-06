b1e44914b26bf8b4df563f5a1f26d441bafae819
Ettiene Mare <ettienemare@gmail.com>
2018-02-06 18:29:06 +0200

    Cleanup the code

--- src/app/modules/app/app.module.ts
+++ src/app/modules/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './components/app/app.component';
+import {PaperModule} from "../paper/paper.module";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PaperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
