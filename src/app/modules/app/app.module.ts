41333da700713a7488f36ea2993d2ddacfae55d0
Ettiene Mare <ettienemare@gmail.com>
2018-02-07 07:51:50 +0200

    Cleanup the code

--- src/app/modules/app/app.module.ts
+++ src/app/modules/app/app.module.ts
@@ -2,16 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app/app.component';
-import {WorkflowModule} from "../../modules/workflow/workflow.module";
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    WorkflowModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
