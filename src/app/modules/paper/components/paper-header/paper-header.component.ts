2c07fe45380271ad0893bd579ea26b293fb32386
Ettiene Mare <ettienemare@gmail.com>
2018-02-15 13:07:46 +0200

    Add nova core and analytics

--- src/app/modules/paper/components/paper-header/paper-header.component.ts
+++ src/app/modules/paper/components/paper-header/paper-header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'paper-header',
   template: `
-      <h1>{{ config.label|translate:model }}</h1>
+      <h1>{{config.label|translate:model}}</h1>
   `
 })
 export class PaperHeaderComponent {
